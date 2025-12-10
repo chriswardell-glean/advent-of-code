cd0ac709e8e14b040c3efde494ca0af578230a75
Chris Wardell <chris.wardell@genio.co>
2025-12-10 14:29:40 +0000

    Day 2 part 2

--- 2025/day2.py
+++ 2025/day2.py
@@ -6,6 +6,7 @@
 # add all invalid IDs up
 
 import os
+import math
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 with open(f"{dir_path}/inputs/day2.txt", "r") as f:
@@ -27,11 +28,17 @@ total_sum = 0
 for id in clean_ids:
     id_str = str(id)
     id_len = len(id_str)
+    halfway = math.floor(id_len / 2)
 
-    if id_len % 2 == 0:
-        halfway = int(id_len / 2)
-        if id_str[0:halfway] == id_str[halfway:]:
-            print("HIT", id_str)
-            total_sum += id
+    for pattern_length in range(1, halfway + 1):
+        # check if this substring length will fit if repeated
+        if id_len % pattern_length == 0:
+            number_of_repeats = int(id_len / pattern_length)
+
+            if id_str == id_str[:pattern_length] * number_of_repeats:
+                print(pattern_length, id_str, id_str[:pattern_length])
+                total_sum += id
+                break
+
 
 print(total_sum)
